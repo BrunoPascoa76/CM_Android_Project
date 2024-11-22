67cfcb1ddfcfa3320e36e413556283477b6d5079
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-11-22 15:30:49 +0000

    temporary commit before going to do something in another project

--- app/src/main/java/cm/project/cmproject/ui/loginScreen.kt
+++ app/src/main/java/cm/project/cmproject/ui/loginScreen.kt
@@ -1,7 +1,8 @@
 package cm.project.cmproject.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -11,12 +12,9 @@ import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,7 +34,7 @@ fun AuthTabs(modifier: Modifier = Modifier) {
     Scaffold(
         modifier = modifier,
         topBar = {
-            Column() {
+            Column{
                 CenterAlignedTopAppBar(
                     title = { Text("Authenticate") },
                     modifier = Modifier.height(50.dp)
@@ -63,7 +61,7 @@ fun AuthTabs(modifier: Modifier = Modifier) {
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier, viewModel: UserViewModel) {
-    Column(modifier = modifier) {
+    Column(modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally,verticalArrangement = Arrangement.Center) {
         Text("Login")
     }
 }
