293f68551340185fcdf21003fdbd3c62e94c17c2
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-12-03 17:22:28 +0000

    fixed some merge stuff

--- app/src/main/java/cm/project/cmproject/ui/navigation/AppNavGraph.kt
+++ app/src/main/java/cm/project/cmproject/ui/navigation/AppNavGraph.kt
@@ -9,10 +9,13 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import cm.project.cmproject.ui.AuthTabs
+import cm.project.cmproject.ui.DeliveryDetailsScreen
 import cm.project.cmproject.ui.HomeScreen
 import cm.project.cmproject.ui.Navbar
 import cm.project.cmproject.ui.OrderScreen
 import cm.project.cmproject.ui.ProfileScreen
+import cm.project.cmproject.ui.QrCodeScannerScreen
+import cm.project.cmproject.viewModels.DeliveryViewModel
 import cm.project.cmproject.viewModels.UserViewModel
 
 
@@ -26,8 +29,9 @@ fun AppNavHost(
     navController: NavHostController = rememberNavController(),
     startDestination: String = "auth"
 ) {
-    val viewModel: UserViewModel = viewModel()
+    val userViewModel: UserViewModel = viewModel()
     val orderViewModel: OrderViewModel = viewModel()
+    val deliveryViewModel: DeliveryViewModel = viewModel()
 
     NavHost(
         navController = navController,
@@ -35,22 +39,34 @@ fun AppNavHost(
         modifier = modifier
     ) {
         composable("auth") {
-            AuthTabs(navController = navController, viewModel = viewModel)
+            AuthTabs(navController = navController, viewModel = userViewModel)
         }
         composable("home") {
             Navbar(navController) {
-                HomeScreen(viewModel=viewModel)
+                HomeScreen(viewModel=userViewModel)
             }
         }
         composable("profile") {
             Navbar(navController) {
-                ProfileScreen(viewModel=viewModel, navController = navController)
+                ProfileScreen(viewModel=userViewModel, navController = navController)
             }
         }
         composable("order") {
             Navbar(navController) {
                 OrderScreen(viewModel=orderViewModel)
             }
+        }
+        composable("deliveryDetails/{deliveryId}"){ backStackEntry ->
+            val deliveryId = backStackEntry.arguments?.getString("deliveryId")?.toIntOrNull()?:0
+            DeliveryDetailsScreen(
+                deliveryId = deliveryId,
+                navController = navController,
+                deliveryViewModel = deliveryViewModel,
+                userViewModel = userViewModel
+            )
+        }
+        composable("qrCodeScanner"){
+            QrCodeScannerScreen(navController=navController, viewModel = deliveryViewModel)
         }
     }
 }
