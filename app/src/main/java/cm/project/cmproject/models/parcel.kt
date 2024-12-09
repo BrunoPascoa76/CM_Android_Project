3c2ffa55836b1aecc7d9d0ae72256beabf1c0842
carlos54vidal <94443213+carlos54vidal@users.noreply.github.com>
2024-12-09 21:55:19 +0000

    Update parcel.kt

--- app/src/main/java/cm/project/cmproject/models/parcel.kt
+++ app/src/main/java/cm/project/cmproject/models/parcel.kt
@@ -2,5 +2,9 @@ package cm.project.cmproject.models
 
 data class Parcel(
     val parcelId: Int=0,
-    val description: String=""
+    val description: String="",
+    val fromAddress: String="",
+    val toAddress: String="",
+    val toEmail: String="",
+    val toPhoneNumber: String="",
 )
